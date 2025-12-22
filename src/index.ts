56aa273eeb7f10a98cc49a2c234f299868a1e397
Avila <pablo.avila@solera.com>
2025-12-22 10:12:16 +0100

    Number of words now showing

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,8 @@ const response = await fetch("https://example.com");
 const newVar = process.env.TEST_ENV
 
 // Use Response as stdin.
-const numberOfWords = $`cat < ${response} | wc -c`; // 1256
+const numberOfWords = await $`cat < ${response} | wc -c`.text(); // 1256
 
 console.log("Number of words: " + numberOfWords);
-
 console.log("Mi variable de entorno de prueba es " + newVar)
 
